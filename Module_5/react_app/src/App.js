d3a87ae4c8850e1e2f3e8f90c6b57747e4335c5e
Divyansh Moonat <divyanshmoonat@outlook.com>
2024-09-13 22:04:09 +0530

    13-09-2024 | Create react app & Vite Boilerplates

---
+++ Module_5/react_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const MyReactApp = () => {
+  return (
+    <div className="App">
+      <h1>This is my first react app with create react app</h1>
+      <p>lorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit ametlorem ipsum dor sit amet lorem ipsum dor sit ametlorem ipsum dor sit amet</p>
+    </div>
+  );
+}
+
+export default MyReactApp;
