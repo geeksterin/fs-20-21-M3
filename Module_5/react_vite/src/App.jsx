a31c87af8929c431dc87da76ef7de7c077efa5ac
Divyansh Moonat <divyanshmoonat@outlook.com>
2024-09-19 22:10:22 +0530

    19-09-2024 | React States

--- Module_5/react_vite/src/App.jsx
+++ Module_5/react_vite/src/App.jsx
@@ -3,16 +3,103 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Footer from "./Footer";
-import Header from "./Header";
-import Content from './Content';
+import Copyright from './Copyright'
 
 const App = () => {
+  // console.log("App component rendered");
+  const [counter, setCounter] = useState(0); // React JS Variable
+  const [userName, setUserName] = useState("John");
+
+  const [year, setYear] = useState(2024);
+
+  // const [bgColor, setBgColor] = useState("#282c34");
+  const [colors, setColors] = useState({
+    bgColor: "#282c34",
+    fontColor: "#ffffff"
+  });
+
+  const onDecreaseButtonClick = () => {
+    if (counter !== 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  const onIncreaseButtonClick = () => {
+    // counter++;
+    setCounter(counter + 1);
+  };
+
+  const onResetButtonClick = () => {
+    setCounter(0);
+  };
+
+  const handleToggleChange = () => {
+    if (userName === "John") {
+      setUserName("Alex")
+    } else {
+      setUserName("John");
+    }
+  }
+
+  const onDarkThemeClick = () => {
+    // setBgColor("#282c34");
+    setColors({
+      bgColor: "#282c34",
+      fontColor: "#ffffff"
+    })
+    console.log("Dark theme clicked")
+  }
+
+  const onLightThemeClicked = () => {
+    // setBgColor("#ffffff");
+    setColors({
+      bgColor: "#ffffff",
+      fontColor: "#282c34"
+    })
+    console.log("Light theme clicked")
+  };
+
+  const onDecreaseYearClick = () => { 
+    setYear(year - 1);
+  };
+
+  const onIncreaseYearClick = () => { 
+    setYear(year + 1);
+  };
+
   return (
-    <div>
-      <Header />
-      <Content />
-      <Footer />
+    <div style={{
+      backgroundColor: colors.bgColor,
+      color: colors.fontColor,
+      width: "100vw",
+      height: "100vh"
+    }}>
+      <h1>Counter App</h1>
+      <div style={{ border: "1px solid black" }}>
+        <button onClick={onDecreaseButtonClick}>Decrease (-)</button>
+        <h2>{counter}</h2>
+        <button onClick={onIncreaseButtonClick}>Increase (+)</button>
+        <button onClick={onResetButtonClick}>Reset</button>
+      </div>
+
+      <div style={{ border: "1px solid black" }}>
+        UserName : {userName}
+        <button onClick={handleToggleChange}>Toggle Name</button>
+      </div>
+      Dark
+      <input onClick={onDarkThemeClick} type='radio' name='theme' />
+      Light
+      <input onClick={onLightThemeClicked} type='radio' name='theme' />
+
+      <div>
+        <button onClick={onDecreaseYearClick}>Decrease Year</button>
+        <button onClick={onIncreaseYearClick}>Increase Year</button>
+      </div>
+      <Copyright year={year} />
+      <Copyright year={year} />
+      <Copyright year={year} />
+      <Copyright year={year} />
+      <Copyright year={year} />
     </div>
   )
 }
--- Module_5/react_vite/src/Copyright.jsx
+++ Module_5/react_vite/src/Copyright.jsx
@@ -1,29 +1,10 @@
-import styles from "./Copyright.module.css";
-
-const Copyright = (props) => {
-    console.log(props)
-    props.nums?.map(data => {
-        console.log(data)
-    })
+const Copyright = ({year}) => {
     return (
-        <div>
-            <p className={styles.largeTxt}>Copyright {props.year}, {props.companyName} , India</p>
-            Details : 
-            Name : {props.details?.userName}
-            Age : {props.details?.age}
-
-            {
-                // Object.entries(props.details).map()
-            }
-            Array Details : 
-            {props.nums?.map(data => <p>{data.userName} {data.id}</p> )}
+        <div style={{
+            fontSize: "25px"
+        }}>
+            Copyright {year}, Meta Inc
         </div>
-    )
+    );
 };
-
 export default Copyright;
-// const copyright = (year) => {
-//     console.log(`Copyright ${year}, XYZ Inc, India`)
-// }
-
-// copyright(2024)
