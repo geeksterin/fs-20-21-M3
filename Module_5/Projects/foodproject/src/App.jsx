19c855486ef3345878db72d098eeb15b445754ed
Divyansh Moonat <divyanshmoonat@outlook.com>
2024-09-23 22:06:59 +0530

    23-09-2024 | Food Project Part 1

---
+++ Module_5/Projects/foodproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Restaurants from './conatiners/Restaurants/Restaurants'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Food Project</h1>
+      <Restaurants />
+    </div>
+  )
+}
+
+export default App
---
+++ Module_5/Projects/foodproject/src/components/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,47 @@
+import styles from "./RestaurantCard.module.css";
+
+const RestaurantCard = (props) => {
+    // console.log(props)
+    return (
+        <div className={styles.container}>
+            <div>
+                <h3 className={styles.title}>
+                    {props.name}
+                </h3>
+                <span>
+                    {
+                        [...new Array(5)].map((item, index) => {
+                            return <i
+                                class={
+                                    `fa-${parseInt(props.rating) >= index + 1 ? "solid" : "regular"
+                                    } fa-star`
+                                }
+                            ></i>
+                        })
+                    }
+                </span>
+            </div>
+            <i class="fa-solid fa-location-dot"></i>
+            <span className={styles.address}>{props.address}</span>
+            <div>
+                {
+                    `${props.outcode} ${props.postcode}`
+                    // props.outcode
+                }
+                {
+                    // props.postcode
+                }
+            </div>
+            <div className={styles["cuisine-container"]}>
+                <div>
+                    <i class="fa-solid fa-utensils"></i>
+                    <span className={styles.cuisine}>{props.type_of_food}</span>
+                </div>
+                <a href="/menu">Visit Menu</a>
+            </div>
+
+        </div>
+    )
+};
+
+export default RestaurantCard;
